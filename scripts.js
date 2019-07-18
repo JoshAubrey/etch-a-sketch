fe8fb6b779237d3f82f101c43cebb5ccd5d87838
JoshAubrey <joshaubrey7@gmail.com>
2019-07-18 16:13:42 -0500

    Add RGB colors w/fade, fix newGrid  prompt Cancel

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
+const container = document.getElementById("container")
 let rows = 32
 let columns = 32
-const container = document.getElementById("container")
+let rgbColors = true
 
 function generateGrid () {
     container.style.display = 'grid'
@@ -19,39 +20,51 @@ function generateGrid () {
             
             div.style.gridRow = row
             div.style.gridColumn = column
-
-            div.style.borderStyle = 'solid'
-            div.style.borderWidth = '1px'
-            div.style.borderColor = 'black'
             
             container.appendChild(div)
             
-            //console.log(row,column)
         }
     }
     
     let gridSquares = document.querySelectorAll('#container div')
     gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseover', () => applyColor(gridSquare)))
-    //gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseout', () => removeColor(gridSquare)))
 
 }
 
 function applyColor (gridSquare) {
-    gridSquare.style.backgroundColor = 'black'
+    if (rgbColors == true) {
+        if (gridSquare.style.backgroundColor == '') {
+            const r = Math.floor(Math.random() * 256)
+            const g = Math.floor(Math.random() * 256)
+            const b = Math.floor(Math.random() * 256)
+            gridSquare.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')'
+        }
+        else {
+            gridSquare.style.backgroundColor = shadeRGBColor(gridSquare.style.backgroundColor, '-.10')
+        }
+    }
+    else {
+        gridSquare.style.backgroundColor = 'black'
+    }
 }
 
-// function removeColor (gridSquare) {
-//     gridSquare.style.backgroundColor = ''
-// }
+function shadeRGBColor(color, percent) {
+    var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+    return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
+}
 
 function newGrid () {
-    container.innerHTML = ''
-    let input = parseInt(prompt("How many squares per side should the new grid be?", "32"))
-    while (Number.isNaN(input)) {
-        console.log(typeof input)
+
+    let input = prompt("How many squares per side should the new grid be?", "32")
+    if (input == null) return
+
+    while (Number.isNaN(parseInt(input))) {
         input = prompt("Please enter a number", "32")
+        if (input == null) return
     }
+
     rows = columns = input
+    container.innerHTML = ''
     generateGrid()
 
 }
@@ -60,6 +73,16 @@ generateGrid()
 
 let newGridButton = document.querySelector('#newGrid')
 newGridButton.addEventListener('click', newGrid)
+
+let rgbCheckbox = document.querySelector('#rgbCheckbox')
+rgbCheckbox.addEventListener('click', (e) => {
+    if (e.target.checked){
+        rgbColors = true
+    }
+    else {
+        rgbColors = false
+    }
+})
 
 
 
